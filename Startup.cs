da8e3ffd0f195bdabd9a9d91e0bd2f5867ae4362
Marc St-Pierre <mstpfirebase@gmail.com>
2018-03-22 23:13:17 -0400

    Extended API

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -33,10 +34,12 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             var key = Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value);
+            services.AddTransient<Seed>();
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc();
             services.AddCors();
+            services.AddAutoMapper();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
@@ -48,10 +51,14 @@ namespace DatingApp.API
                         ValidateAudience = false
                     };
                 });
+            services.AddMvc().AddJsonOptions(opt =>
+            {
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -74,6 +81,7 @@ namespace DatingApp.API
                     });
                 });
             }
+            // seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseAuthentication();
             app.UseMvc();
--- notes.md
+++ notes.md
@@ -10,3 +10,21 @@ dotnet restore
 export ASPNETCORE_ENVIRONMENT=development 
 
 // test
+
+
+
+
+If your migration involves renaming a column in the table, then this will not work with SQLite due to its limitations, more details here.  I presume you have renamed one of the properties in a model?
+https://docs.microsoft.com/en-us/ef/core/providers/sqlite/limitations
+
+The easiest fix to this situation is to do the following:
+
+1. Drop the existing database (dotnet ef database drop)
+
+2. Delete the migrations folder to remove any existing migrations.
+
+3. Create a new migration (dotnet ef migrations add initialCreate)
+
+4. Run dotnet ef database update.
+
+This will resolve the issue.
